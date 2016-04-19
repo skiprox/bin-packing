cb40601144fa13ddebf9e6d310de9897d78dce22
Sean Scanlan <sean.scanlan@kettlenyc.com>
2016-04-19 16:48:23 -0400

    Adding more sizes to the example

--- examples/main.js
+++ examples/main.js
@@ -9,16 +9,16 @@ var Main = (function() {
 		init : function() {
 			var container = document.querySelector('.container');
 			var colors = ['red', 'pink', 'green', 'blue', 'orange'];
-			var sizes = [40, 80, 120, 160, 200];
+			var sizes = [40, 80, 120, 160, 200, 240, 280, 320];
 			var sizesLen = sizes.length;
 			var blocks = [];
 			var i = 0;
-			for (i; i < 100; i++) {
+			for (i; i < 500; i++) {
 				blocks[i] = document.createElement('div');
 				blocks[i].setAttribute('class', 'block');
 				blocks[i].style.backgroundColor = colors[i % 5];
-				blocks[i].style.width = sizes[Math.floor(Math.random() * 5)] + 'px';
-				blocks[i].style.height = sizes[Math.floor(Math.random() * 5)] + 'px';
+				blocks[i].style.width = sizes[Math.floor(Math.random() * 8)] + 'px';
+				blocks[i].style.height = sizes[Math.floor(Math.random() * 8)] + 'px';
 				container.appendChild(blocks[i]);
 			}
 			var binPacks = new BinPacking('.container', '.block');
